1a2a28cf6556dd77e175b137e41f3114ce885cab
Luis Cossío <luis.cossio@outlook.com>
2022-04-30 13:56:41 -0500

    change arrows to eggdogs

---
+++ src/client/boid.ts
@@ -0,0 +1,161 @@
+import * as THREE from 'three'
+
+export abstract class Boid extends THREE.Object3D {
+    velocity = new THREE.Vector3(0, 0, 0)
+    acceleration = new THREE.Vector3(0, 0, 0)
+    maxForce = 0.4
+    maxSpeed = 0.5
+    width = 80
+    height = 50
+
+    constructor() {
+        super()
+        this.position.set(
+            (Math.random() - 0.5) * this.width,
+            0,
+            (Math.random() - 0.5) * this.height,
+        )
+        this.velocity.set(
+            (Math.random() - 0.5) * 0.1,
+            0,
+            (Math.random() - 0.5) * 0.1,
+        )
+        this.acceleration.copy(this.acceleration)
+    }
+
+    update(otherBoids: Boid[], strength = 0.006) {
+        otherBoids = this.getNeighbors(otherBoids, 15)
+        this.separate(otherBoids, 0.4 * strength)
+        this.align(otherBoids, 1.7 * strength)
+        this.cohese(otherBoids, 0.7 * strength)
+        this.move()
+        this.constrain(80, 50)
+    }
+
+    /**
+     *
+     * @param neighbors An array of other boids that do not include this boid
+     * @param strength How strong is the boid's separation force
+     * @returns void
+     */
+    separate(neighbors: Boid[], strength: number = 0.003): void {
+        // if there are no boids nearby, return
+        if (neighbors.length === 0) {
+            return
+        }
+        // get the sum of distances to the other boids
+        let steering = new THREE.Vector3(0, 0, 0)
+        for (const other of neighbors) {
+            let diff = this.position.clone().sub(other.position)
+            // inversely proportional to the distance
+            diff.divideScalar(other.position.distanceTo(this.position))
+            steering.add(diff)
+        }
+        // steering.divideScalar(neighbors.length)
+        steering.multiplyScalar(strength)
+
+        this.acceleration.add(steering)
+    }
+
+    /**
+     * Steers towards the average heading of the local flock
+     *
+     * @param neighbors An array of other neighboring boids
+     * @returns void
+     */
+    align(neighbors: Boid[], strength: number = 0.011): void {
+        let steering = new THREE.Vector3(0, 0, 0)
+
+        if (neighbors.length === 0) {
+            return
+        }
+
+        for (const other of neighbors) {
+            steering.add(other.velocity)
+        }
+        // Steer towards the average direction
+        steering.divideScalar(neighbors.length)
+        steering.multiplyScalar(strength)
+
+        this.acceleration.add(steering)
+    }
+
+    /**
+     * Move towards the center of the local flock
+     * @param otherBoids An array of other boids that do not include this boid
+     * @param neighborDistance The distance to other boids that will be considered neighbors
+     * @returns void
+     */
+    cohese(neighbors: Boid[], strength: number = 0.002): void {
+        if (neighbors.length === 0) {
+            return
+        }
+        let avgPosition = new THREE.Vector3(0, 0, 0)
+
+        for (const other of neighbors) {
+            avgPosition.add(other.position)
+        }
+        avgPosition.divideScalar(neighbors.length)
+
+        // steer towards the average position
+        let steering = avgPosition.sub(this.position)
+        steering.multiplyScalar(strength)
+        this.acceleration.add(steering)
+    }
+
+    /**
+     * Updates the position of the boid using the current velocity and acceleration
+     * @returns void
+     */
+    move() {
+        this.position.add(this.velocity)
+        this.velocity.add(this.acceleration)
+        this.velocity.clampLength(0, this.maxSpeed)
+        this.acceleration.clampLength(0, this.maxForce)
+        this.lookAt(this.position.clone().sub(this.velocity))
+    }
+
+    /**
+     * Constrains the boid to the bounds of the scene
+     * @returns void
+     */
+    constrain(width: number = 100, height: number = 50, depth: number = 50) {
+        if (this.position.x > width) {
+            this.position.x = -width
+        }
+        if (this.position.x < -width) {
+            this.position.x = width
+        }
+        if (this.position.y > height) {
+            this.position.y = -height
+        }
+        if (this.position.y < -height) {
+            this.position.y = height
+        }
+        if (this.position.z > depth) {
+            this.position.z = -depth
+        }
+        if (this.position.z < -depth) {
+            this.position.z = depth
+        }
+    }
+
+    /**
+     * Gets the neighboring boids that can be perceived by this boid
+     * @param otherBoids An array of boids
+     * @param distance the maximum distance to be perceived by the boid
+     * @param fov the angle of the boid's field of view, with respect to the pointing direction
+     * @returns
+     */
+    getNeighbors(otherBoids: Boid[], distance: number = 10, fov: number = Math.PI / 1.5) {
+        return otherBoids.filter((other) => {
+            const vectorToOther = other.position.clone().sub(this.position)
+            const angleToOther = this.velocity.angleTo(vectorToOther)
+            return (
+                other !== this &&
+                this.position.distanceTo(other.position) < distance &&
+                angleToOther < fov
+            )
+        })
+    }
+}
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Flock } from './flock'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene()
 
@@ -14,26 +13,9 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
-/*const loader = new GLTFLoader();
-
-loader.load( 'EggDog.glb', function ( glb ) {
-    console.log(glb)
-
-    const root = glb.scene;
-    scene.add( root );
-
-}, undefined, function ( error ) {
-
-    console.error( error );
-
-} );*/
-//////////////  luz  //////////////////////
-scene.background = new THREE.Color( 0xa0a0a0 );
+const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+hemiLight.position.set( 0, 20, 0 );
+scene.add(hemiLight);
 
 const dirLight = new THREE.DirectionalLight( 0xffffff );
 dirLight.position.set( - 3, 10, - 10 );
@@ -44,12 +26,11 @@ dirLight.shadow.camera.left = - 2;
 dirLight.shadow.camera.right = 2;
 dirLight.shadow.camera.near = 0.1;
 dirLight.shadow.camera.far = 40;
-scene.add( dirLight );
-////////////////////////////////////
-//const cube = new THREE.Mesh(geometry, material)
-//scene.add(cube)
-const flock = new Flock(100);
-scene.add(flock);
+                
+scene.add(dirLight);
+                
+const flock = new Flock(40)
+scene.add(flock)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -62,9 +43,7 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate)
 
-    flock.update();
-    //cube.rotation.x += 0.01
-    //cube.rotation.y += 0.01
+    flock.update()
 
     controls.update()
 
