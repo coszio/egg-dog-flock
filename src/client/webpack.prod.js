1e32b695d1b47f62a28acdc160afb0b985747e31
Luis Cossío <luis.cossio@outlook.com>
2022-06-01 09:22:17 -0500

    hotfix: add assets to gh-pages

--- src/client/webpack.prod.js
+++ src/client/webpack.prod.js
@@ -1,9 +1,22 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+ 
+// module.exports = {
+//     context: path.join(__dirname, 'your-app'),
+    
+// };
 
 module.exports = merge(common, {
     mode: 'production',
     performance: {
         hints: false
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public' }
+            ]
+        })
+    ]
 });
